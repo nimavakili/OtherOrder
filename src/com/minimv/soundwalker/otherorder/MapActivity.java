1390a4c81661ee9c4a11a77f44a39ed25f5729bc
U-Nima-PC\Nima <Nima@Nima-PC.(none)>
2014-09-06 17:40:45 -0400

    v3.1 in progress

---
+++ src/com/minimv/soundwalker/otherorder/MapActivity.java
@@ -0,0 +1,14 @@
+package com.minimv.soundwalker.otherorder;
+
+import com.minimv.soundwalker.otherorder.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_map);
+    }
+}
